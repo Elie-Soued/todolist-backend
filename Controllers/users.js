0b9f8e868026237c0ddb45d8e97cc9395a472972
Elie Soued <eliegebransoued@gmail.com>
2022-09-26 19:58:48 +0300

    create users controllers

--- Controllers/users.js
+++ Controllers/users.js
@@ -1,14 +1,17 @@
 const pool = require("../dbconfig");
+const bcrypt = require("bcrypt");
 
 module.exports = {
-  create: async (req, res) => {
+  register: async (req, res) => {
     const { username, password } = req.body;
 
     try {
+      const hashedPassword = await bcrypt.hash(password, 10);
       const queryString =
         'INSERT INTO "users" (username, password) VALUES ($1,$2);';
       const dbResponse = await pool.query(queryString, [
-        { username, password },
+        username,
+        hashedPassword,
       ]);
       res.json({
         code: 200,
@@ -23,73 +26,75 @@ module.exports = {
     }
   },
 
-  //   getAll: async (_, res) => {
-  //     try {
-  //       const dbResponse = await pool.query("SELECT * FROM todos");
-  //       res.json({
-  //         code: 200,
-  //         message: "success",
-  //         data: dbResponse.rows,
-  //       });
-  //     } catch (e) {
-  //       console.error(Error(e));
-  //       res.status(500);
-  //     }
-  //   },
+  login: async (req, res) => {
+    const { username, password } = req.body;
 
-  //   updateById: async (req, res) => {
-  //     const { id } = req.params;
-  //     const { todo } = req.body;
+    const users = await pool.query("SELECT * FROM users");
+    const result = users.rows;
+    const user = result.find((user) => user.username === username);
 
-  //     try {
-  //       const queryString = "UPDATE todos SET todo = $1 WHERE id = $2";
+    if (user === null) {
+      return res.status(400).send("cannot find user");
+    }
 
-  //       await pool.query(queryString, [todo, id]);
-  //       res.json({
-  //         code: 200,
-  //         message: "updated todo correcty with id" + id,
-  //       });
-  //     } catch (e) {
-  //       res.status(500).json({
-  //         code: 500,
-  //         message: "Error trying to update todo with id" + id,
-  //       });
-  //     }
-  //   },
+    try {
+      if (bcrypt.compare(password, user.password)) {
+        res.send("success");
+      } else {
+        res.send("Not Allowed");
+      }
+    } catch (e) {
+      res.status(500).send();
+    }
+  },
 
-  //   deleteById: async (req, res) => {
-  //     const { id } = req.params;
+  getAll: async (_, res) => {
+    try {
+      const dbResponse = await pool.query("SELECT * FROM users");
+      res.json({
+        code: 200,
+        message: "success",
+        data: dbResponse.rows,
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500);
+    }
+  },
 
-  //     try {
-  //       const queryString = 'DELETE FROM "todos" WHERE id=$1';
-  //       await pool.query(queryString, [id]);
-  //       res.json({
-  //         code: 200,
-  //         message: "deleted todo correctly with id " + id,
-  //       });
-  //     } catch (e) {
-  //       console.error(Error(e));
-  //       res.status(500).json({
-  //         code: 500,
-  //         message: "Error trying to delete a todo with id " + id,
-  //       });
-  //     }
-  //   },
+  deleteById: async (req, res) => {
+    const { id } = req.params;
 
-  //   getByID: async (req, res) => {
-  //     const { id } = req.params;
-  //     try {
-  //       const dbResponse = await pool.query("SELECT * FROM todos WHERE id=$1", [
-  //         id,
-  //       ]);
-  //       res.json({
-  //         code: 200,
-  //         message: "success. Found todo with id " + id,
-  //         data: dbResponse.rows[0],
-  //       });
-  //     } catch (e) {
-  //       console.error(Error(e));
-  //       res.status(500);
-  //     }
-  //   },
+    try {
+      const queryString = 'DELETE FROM "users" WHERE id=$1';
+      await pool.query(queryString, [id]);
+      res.json({
+        code: 200,
+        message: "deleted user correctly with id " + id,
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500).json({
+        code: 500,
+        message: "Error trying to delete a user with id " + id,
+      });
+    }
+  },
+
+  getByID: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const dbResponse = await pool.query("SELECT * FROM users WHERE id=$1", [
+        id,
+      ]);
+      res.json({
+        code: 200,
+        message: "success. Found user with id " + id,
+        data: dbResponse.rows[0],
+      });
+    } catch (e) {
+      console.error(Error(e));
+      res.status(500);
+    }
+  },
 };
