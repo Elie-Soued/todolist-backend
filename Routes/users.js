2fafdc411c0a24a82b840e75da3ac3b64c2f646d
Elie Soued <eliegebransoued@gmail.com>
2022-10-09 17:29:50 +0300

    remove useless routes

--- Routes/users.js
+++ Routes/users.js
@@ -1,17 +1,8 @@
-//import express
 const express = require("express");
-//give router the value express.Router()
 const router = express.Router();
-
-//import the usersControllers
 const usersControllers = require("../Controllers/users");
 
-//Setting the user Routes (associating a function/Controller/Middleware to an endpoint)
-router.get("/:id", usersControllers.getByID);
-router.get("/", usersControllers.getAll);
 router.post("/", usersControllers.register);
 router.post("/login", usersControllers.login);
-router.delete("/:id", usersControllers.deleteById);
 
-//export the user Routes
 module.exports = router;
